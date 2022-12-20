a108e0a2345648d9a683e35b160d8b33190e8215
Gabriel Trudes Melo <esquecimeunome00@hotmail.com>
2022-12-20 20:11:45 -0300

    Curso dois de Dart orientado a objeto

---
+++ bin/dart_curso_dois.dart
@@ -0,0 +1,38 @@
+void main() {
+  String nome = "Laranja";
+  double peso = 100.2;
+  String cor = "Verde e Amarela";
+  String sabor = "Doce e cítrica";
+  int diasDesdeColheita = 40;
+  bool isMadura = funcEstaMadura(diasDesdeColheita);
+
+  //mostrarMadura("Uva", 40, cor: "roxo");
+  int quantosDias = funcQuantosDiasMadura(diasDesdeColheita);
+  print(quantosDias);
+}
+
+int funcQuantosDiasMadura(int dias) {
+  int diasParaMadura = 30;
+  int quantosDiasFaltam = dias - diasParaMadura;
+  return quantosDiasFaltam;
+}
+
+void mostrarMadura(String nome, int dias, {required String cor}) {
+  if (dias >= 30) {
+    print("A $nome está madura.");
+  } else {
+    print("A $nome não está madura.");
+  }
+
+  if (cor != null) {
+    print("A $nome é $cor.");
+  }
+}
+
+bool funcEstaMadura(int dias) {
+  if (dias >= 30) {
+    return true;
+  } else {
+    return false;
+  }
+}
